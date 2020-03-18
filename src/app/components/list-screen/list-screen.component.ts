9ebb92104db499ad0d35576385a4f5077f7dbf64
ajay-3 <ajay.hn3@gmail.com>
2020-03-18 17:36:17 +0530

    edited

--- src/app/components/list-screen/list-screen.component.ts
+++ src/app/components/list-screen/list-screen.component.ts
@@ -9,59 +9,14 @@ import {EmployeeService} from '../../services/employee.service'
   styleUrls: ['./list-screen.component.css']
 })
 export class ListScreenComponent implements OnInit {  
- employees = [];
-//   employees = [
-//      { "Name":"Ajay",
-//   "Salary":6,
-//   "DOB":"13-05-1996",
-//   Skills:[{"value":"Skill 2","id":2,"checked":true},{"value":"skill 7","id":7,"checked":true}],
-//   Id:28,
-//   imageUrl:"https://employee2728.s3.us-east-2.amazonaws.com/1584459336281tommy-lisbin-xr-y6Ruw7K8-unsplash.jpg"
-// },
-//   { "Name":"prabhu",
-//   "Salary":6,
-//   "DOB":"13-05-1996",
-//   Skills:[{value:"Skill 0",id:0,checked:true},{value:"Skill 3",id:3,checked:true},{value:"Skill 5",id:5,checked:true},],
-//   Id:40,
-//   imageUrl:"https://employee2728.s3.us-east-2.amazonaws.com/1584440272163204-304-Stainless-Steel-Sheet-Price-Per.png"},
-//   { "Name":"Ravi",
-//   "Salary":6,
-//   "DOB":"13-05-1996",
-//   Skills:[{"value":"Skill 2","id":2},{"value":"skill 7","id":7}],
-//   Id:27,
-//   imageUrl:"https://employee2728.s3.us-east-2.amazonaws.com/1584459336281tommy-lisbin-xr-y6Ruw7K8-unsplash.jpg"},
-//   { "Name":"Shashi",
-//   "Salary":6,
-//   "DOB":"13-05-1996",
-//   Skills:[{"value":"Skill 2","Id":2},{"value":"skill 7","id":7}],
-//   Id:101,
-//   imageUrl:"https://employee2728.s3.us-east-2.amazonaws.com/1584440066233sherry-christian-8Myh76_3M2U-unsplash.jpg"},
-//   { "Name":"Pramod",
-//   "Salary":6,
-//   "DOB":"13-05-1996",
-//   Skills:[{"value":"Skill 2","Id":2},{"value":"skill 7","id":7}],
-//   Id:43,
-//   imageUrl:"https://employee2728.s3.us-east-2.amazonaws.com/1584363650726Mamatha_CV-3.pdf"},
-//   { "Name":"Ajay",
-//   "Salary":6,
-//   "DOB":"13-05-1996",
-//   Skills:[{"value":"Skill 2","Id":2},{"value":"skill 7","id":7}],
-//   Id:28,
-//   imageUrl:"https://employee2728.s3.us-east-2.amazonaws.com/1584363650726Mamatha_CV-3.pdf"},
-//   { "Name":"Ajay",
-//   "Salary":6,
-//   "DOB":"13-05-1996",
-//   Skills:[{"value":"Skill 2","Id":2},{"value":"skill 7","id":7}],
-//   Id:28,
-//   imageUrl:"https://employee2728.s3.us-east-2.amazonaws.com/1584363650726Mamatha_CV-3.pdf"}
-// ]
-
+employees = [];
 pageIndex: number;
 pageSize: number;
 size;
-SearchFormControl = new FormControl('', [
-  Validators.minLength(2)
-]);
+noSearch=false;
+error=false;
+errorValue;
+
   constructor(private router:Router,private employeeService:EmployeeService) {
     this.pageIndex = 0;
     this.pageSize = 1;
@@ -69,29 +24,55 @@ SearchFormControl = new FormControl('', [
 
   ngOnInit() {
    this.employeeService.getAllEmployees().subscribe((res)=>{ 
-     console.log(res)
+      this.employees = Object.values(res);
+      this.size = this.employees.length;
     });
-    this.size = this.employees.length;
+    
   }
+
+  SearchFormControl = new FormControl('', [
+    Validators.minLength(2)
+  ]);
+
+  onSubmit(){
+    this.employeeService.searchEmployees({"searchValue":this.SearchFormControl.value}).subscribe((data)=>{
+      // this.employeeService.searchUsers(data); 
+       this.employees.splice(0,this.employees.length-1);
+       this.employees = Object.values(data);
+       if(this.employees.length == 0){
+           this.noSearch = true;
+       }else{
+        this.size = this.employees.length
+       }
+      }
+    )
+    //this.router.navigate(["/search"]);
+    this.SearchFormControl.reset();
+  }
+ 
 
   edit(item){
     this.employeeService.editClick(item);
     this.router.navigate(["/edit-employee"])
   }
 
-  delete(Id){
-      this.employeeService.deleteEmployee({"Id":Id}).subscribe((res)=>{console.log(res)});
+  delete(item){
+      this.employeeService.deleteEmployee({"Id":item.Id}).subscribe((res)=>{
+        if(res["ok"]==1){
+          var indx=this.employees.indexOf(item);
+          this.employees.splice(indx,1);
+          this.size = this.employees.length;
+        }else{
+          this.error=true;
+          this.errorValue = "Something went wrong"
+        }}
+);
   }
 
-  add(){
-    this.router.navigate(["/add-employee"])
+  back(){
+    this.employeeService.getAllEmployees().subscribe((res)=>{ 
+      this.employees = Object.values(res);
+      this.size = this.employees.length;
+    });
   }
-
-  onSubmit(){
-    this.employeeService.searchEmployees({"searchValue":this.SearchFormControl.value}).subscribe(
-      (data)=>{console.log(data)}
-    )
-    this.SearchFormControl.reset();
-  }
-  
 }
